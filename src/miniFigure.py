f887b57bda89ad5dd518cfbede53bdfe8f555ce8
Vladutul <ciurdeavladut@gmail.com>
2025-04-26 17:53:48 +0300

    update_26_04_25_17_54

--- src/miniFigure.py
+++ src/miniFigure.py
@@ -15,31 +15,89 @@ class createMiniFigure:
         self.view_miniFigure.setCameraPosition(distance=20)
         self.items_miniFigure = []
 
-    def create_miniFigure_parallelipiped(self):
+    def create_miniFigure_drillPlate(self):
         id = len(self.plot_shapes_values_dictionary)
         key = f"id_{id}"
 
-        x1, x2, y1, y2, z1, z2 = self.generate_unused_real_values_parallelipiped()
-        color = (0, 3, 1, 0.4)
+        x_center, y_center, z_min, height, radius = self.generate_unused_real_values_roundHole()
+        color = (1, 0, 0, 0.8)
 
         self.plot_shapes_values_dictionary[key] = {
-            'shape': 'parallelipiped',
-            'real_values': (x1, x2, y1, y2, z1, z2, color),
+            'shape': 'drillPlate',
+            'real_values': (x_center, y_center, z_min, height, radius, color),
             'top_mask': None,
             'bottom_mask': None,
             'visibleState': False
         }
 
-        self.plot_miniFigure_parallelipiped(x1, x2, y1, y2, z1, z2, color)
+        self.plot_miniFigure_roundHole(x_center, y_center, z_min, height, radius, color)
         self.input_boxes[key] = {}
 
+        mini_widget, mini_grid_layout = self.create_mini_widget_layout()
+
+        labels = ['x_min', 'x_max', 'y_min', 'y_max', 'z_min', 'z_max']
+        default_values = [x_center, y_center, z_min, height, radius]
+
+        show_button = self.classUIinitialization.create_button(
+            lambda k=key: self.change_visibleState(k), mini_grid_layout, "Show", 0, 0)
+        remove_button = self.classUIinitialization.create_button(
+            lambda k=key, w=mini_widget: self.remove_shape(k, w), mini_grid_layout, "Remove", 1, 0)
+
+        show_button.setFixedSize(50, 25)
+        remove_button.setFixedSize(50, 25)
+
+        for i, (label, value) in enumerate(zip(labels, default_values), start=2):
+            line_edit = QLineEdit(str(value))
+            line_edit.setFixedWidth(50)
+            self.input_boxes[key][label] = line_edit
+            mini_grid_layout.addWidget(line_edit, i, 0)
+            line_edit.editingFinished.connect(lambda l=label, k=key: self.edit_shapeValues_roundHole(k))
+
+        mini_grid_layout.addWidget(self.view_miniFigure, 0, 1, 7, 1)
+        self.parent_layout.addWidget(mini_widget)
+
+    def create_mini_widget_layout(self):
         mini_grid_layout = QGridLayout()
         mini_widget = QWidget()
         mini_widget.setLayout(mini_grid_layout)
         mini_widget.setFixedSize(300, 250)
+        return mini_widget, mini_grid_layout
+
+    def create_shape_real_values(self, key, values, shape_type):
+        if shape_type == 'parallelipiped' or shape_type == 'drillPlate':
+            x_min, x_max, y_min, y_max, z_min, z_max, color = values
+            self.plot_shapes_values_dictionary[key] = {
+                'shape': shape_type,
+                'real_values': (x_min, x_max, y_min, y_max, z_min, z_max, color),
+                'top_mask': None,
+                'bottom_mask': None,
+                'visibleState': False
+            }
+        elif shape_type == 'roundHole':
+            x_center, y_center, z_min, height, radius, color = values
+            self.plot_shapes_values_dictionary[key] = {
+                'shape': shape_type,
+                'real_values': (x_center, y_center, z_min, height, radius, color),
+                'top_mask': None,
+                'bottom_mask': None,
+                'visibleState': False
+            }
+        
+        return self.plot_shapes_values_dictionary[key]
+
+    def create_miniFigure_parallelipiped(self):
+        id = len(self.plot_shapes_values_dictionary)
+        key = f"id_{id}"
+
+        values = self.generate_unused_real_values_parallelipiped()
+        self.plot_shapes_values_dictionary[key] = self.create_shape_real_values(key, values, 'parallelipiped')
+        self.plot_miniFigure_parallelipiped(*values[:-1], color=values[-1])
+        self.input_boxes[key] = {}
+
+        mini_widget, mini_grid_layout = self.create_mini_widget_layout()
 
         labels = ['x1', 'x2', 'y1', 'y2', 'z1', 'z2']
-        default_values = [x1, x2, y1, y2, z1, z2]
+        default_values = [values[:-1]]
 
         show_button = self.classUIinitialization.create_button(
             lambda k=key: self.change_visibleState(k), mini_grid_layout, "Show", 0, 0)
@@ -122,27 +180,15 @@ class createMiniFigure:
         id = len(self.plot_shapes_values_dictionary)
         key = f"id_{id}"
 
-        x_center, y_center, z_min, height, radius = self.generate_unused_real_values_roundHole()
-        color = (1, 0, 0, 0.8)
-
-        self.plot_shapes_values_dictionary[key] = {
-            'shape': 'roundHole',
-            'real_values': (x_center, y_center, z_min, height, radius, color),
-            'top_mask': None,
-            'bottom_mask': None,
-            'visibleState': False
-        }
-
-        self.plot_miniFigure_roundHole(x_center, y_center, z_min, height, radius, color)
+        values = self.generate_unused_real_values_roundHole()
+        self.plot_shapes_values_dictionary[key] = self.create_shape_real_values(key, values, 'roundHole')
+        self.plot_miniFigure_roundHole(*values[:-1], color=values[-1])
         self.input_boxes[key] = {}
 
-        mini_grid_layout = QGridLayout()
-        mini_widget = QWidget()
-        mini_widget.setLayout(mini_grid_layout)
-        mini_widget.setFixedSize(300, 250)
+        mini_widget, mini_grid_layout = self.create_mini_widget_layout()
 
         labels = ['x_center', 'y_center', 'z_min', 'height', 'radius']
-        default_values = [x_center, y_center, z_min, height, radius]
+        default_values = [*values[:-1]]
 
         show_button = self.classUIinitialization.create_button(
             lambda k=key: self.change_visibleState(k), mini_grid_layout, "Edit", 0, 0)
@@ -229,6 +275,13 @@ class createMiniFigure:
         widget.setParent(None)
 
     def create_masks_for_real_values(self, key):
+        if self.plot_shapes_values_dictionary[key]['shape'] == 'parallelipiped':
+            pass
+        elif self.plot_shapes_values_dictionary[key]['shape'] == 'roundHole':
+            pass
+        elif self.plot_shapes_values_dictionary[key]['shape'] == 'drillPlate':
+            pass
+
         real_values = self.plot_shapes_values_dictionary[key]['real_values']
         x_min, x_max, y_min, y_max, z_min, z_max, color = real_values
 
@@ -244,47 +297,43 @@ class createMiniFigure:
         self.items_miniFigure.clear()
 
     def generate_unused_real_values_parallelipiped(self):
-        # Extract all existing x_min and x_max ranges from real_values
+        color = (0, 3, 1, 0.4)
         existing_ranges = []
         for key, data in self.plot_shapes_values_dictionary.items():
             real_values = data.get('real_values', [])
-            if len(real_values) >= 2:  # Ensure there are at least x_min and x_max
+            if len(real_values) >= 2:
                 x_min, x_max = real_values[:2]
                 existing_ranges.append((x_min, x_max))
 
-        # Generate new x_min and x_max values that do not overlap with existing ranges
         x_min, x_max = -10, -3
         while any(x_min <= existing_x_max and x_max >= existing_x_min for existing_x_min, existing_x_max in existing_ranges):
-            x_min -= 6  # Shift the range further left
+            x_min -= 6
             x_max -= 6
 
-        # Default values for y_min, y_max, z_min, and z_max
         y_min, y_max = 1, 7
         z_min, z_max = 0, 2.5
 
-        return x_min, x_max, y_min, y_max, z_min, z_max
+        return x_min, x_max, y_min, y_max, z_min, z_max, color
 
     def generate_unused_real_values_roundHole(self):
-        # Extract all existing x_center and y_center values from real_values
+        color = (1, 0, 0, 0.8)
         existing_centers = []
         for key, data in self.plot_shapes_values_dictionary.items():
-            if data.get('shape') == 'roundHole':  # Only consider round holes
+            if data.get('shape') == 'roundHole':
                 real_values = data.get('real_values', [])
-                if len(real_values) >= 2:  # Ensure there are at least x_center and y_center
+                if len(real_values) >= 2:
                     x_center, y_center = real_values[:2]
                     existing_centers.append((x_center, y_center))
 
-        # Generate new x_center and y_center values that do not overlap with existing centers
         x_center, y_center = -3, 15
         while any(abs(x_center - existing_x) < 2 and abs(y_center - existing_y) < 2 for existing_x, existing_y in existing_centers):
-            x_center -= 5  # Shift x_center further left
+            x_center -= 5
 
-        # Default values for z_min, height, and radius
         z_min = 0
         height = 2.6
         radius = 2
 
-        return x_center, y_center, z_min, height, radius
+        return x_center, y_center, z_min, height, radius, color
 
     def change_visibleState(self, key):
         if key in self.plot_shapes_values_dictionary:
--- src/shapeManipulation.py
+++ src/shapeManipulation.py
@@ -48,7 +48,7 @@ class shapeManipulationClass(QWidget):
         self.btn_clear = self.parent.create_button(self.clear_plot, self.buttons_horizontal_layout_first_row, "Șterge Graficul")
         self.create_roundFigure = self.parent.create_button(self.roundShape_figure_wrapper, self.buttons_horizontal_layout_first_row, "Gaura")
         self.create_parallelipipedFigure = self.parent.create_button(self.parallelipipedShape_figure_wrapper, self.buttons_horizontal_layout_first_row, "Paralelipiped")
-
+        self.create_drillPlateFigure = self.parent.create_button(self.parallelipipedShape_figure_wrapper, self.buttons_horizontal_layout_first_row, "DrillPlate")
         self.setLayout(self.main_grid_layout)
     
     def update_plot(self):
@@ -56,7 +56,7 @@ class shapeManipulationClass(QWidget):
 
     def clear_plot(self):
         self.plotManager.clear_plot()
-
+    
     def parallelipipedShape_figure_wrapper(self):
         mini = self.mini_create_wrapper()
         mini.create_miniFigure_parallelipiped()
